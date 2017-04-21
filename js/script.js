bc1933dc399504dbfaff0250dd354bc9f5ca5551
Calum Johnston <calumjohnston2@gmail.com>
2017-04-21 18:59:44 +1000

    World's worst text to speech
    1.0

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var i = 0;
+
+function myLoop() {
+  var split = $("#text").val().split(/\s+/);
+  setTimeout(function() {
+    var currentWord = split[i];
+    $("div").append('<audio autoplay="autoplay" controls="controls" class="' + i + '">  <source       src="http://ssl.gstatic.com/dictionary/static/sounds/de/0/' + currentWord + '.mp3" />  </audio>');
+    i++;
+    if (i < split.length) {
+      myLoop();
+    } else if (i >= split.length || currentWord == undefined) {
+      i = 0;
+    }
+  }, 400)
+}
+
+$('button').on('click', function() {
+  myLoop();
+});
+
+$("#text").keyup(function(event){
+    if(event.keyCode == 13){
+        $("button").click();
+    }
+});
