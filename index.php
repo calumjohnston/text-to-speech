b234e4b0b49d745803682ed870af079030e514dc
Calum Johnston <calumjohnston2@gmail.com>
2017-04-29 16:54:16 +1000

    Update
    Added ability to share links with friends

--- index.php
+++ index.php
@@ -9,11 +9,31 @@
 	
 </head>
 <body>
-<div class="wrapper">
-<input type="text" id="text" placeholder="Type your text">
-<button>Listen</button>
-<div></div>
+<?php
+	if (isset($_GET['w'])){
+	$text = preg_replace('/[^-a-zA-Z0-9_ ]/', '', $_GET['w']);
+	}
+	else{
+		$text="";
+	}
+?>
+<div class="modal hidden">
+ <div class="modal-content">
+    <p id="share-title">Share link<span id="close">&times;</span></p>
+    <div class="share-info">
+    	<span id="share-link"></span>
+    	<!--<button id="copy-url">Copy to Clipboard</button>-->
+    </div>
+  </div>
 </div>
+<div class="wrapper">
+<input type="text" id="text" placeholder="Type your text" value="<?php echo $text ?>">
+<button id="listen">Listen</button>
+<button id="save">Share</button>
+<div></div>
+
+</div>
+
 <script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,25 +1,74 @@
 var i = 0;
 
-function myLoop() {
+function playAudio() {
   var split = $("#text").val().split(/\s+/);
   setTimeout(function() {
     var currentWord = split[i];
     $("div").append('<audio autoplay="autoplay" controls="controls" class="' + i + '"><source src="http://ssl.gstatic.com/dictionary/static/sounds/de/0/' + currentWord + '.mp3" /></audio>');
     i++;
     if (i < split.length) {
-      myLoop();
+      playAudio();
     } else if (i >= split.length || currentWord == undefined) {
       i = 0;
     }
-  }, 400)
+  }, 420)
 }
 
-$('button').on('click', function() {
-  myLoop();
+$('#listen').on('click', function() {
+  playAudio();
 });
 
+
+//On click of Save button, store words in URL
+$('#save').on('click', function() {
+  var words = $("#text").val().split(/\s+/);
+  var wordString= "";
+  for (var i = 0; i < words.length; i++) {
+    if (i==0){
+      var wordString = wordString + words[i];
+    }
+    else{
+      var wordString = wordString + "+" + words[i];
+    }
+    window.history.pushState('Saved Text', 'Title', '?w=' + wordString);
+    //trigger modal
+    fireModal();
+  };
+  
+});
+
+$('a').bind('click', function(e){
+  e.preventDefault();
+});
+
+//Modal
+
+function fireModal(){
+    $("#share-link").html('<a href="' + window.location.href +'">'+ window.location.href+'</a>');
+    $(".modal").fadeIn("fast");
+  $('#close',"").on('click', function() {
+    $(".modal").fadeOut("fast");
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Enter Key Listen
 $("#text").keyup(function(event){
     if(event.keyCode == 13){
-        $("button").click();
+        $("#listen").click();
     }
 });
